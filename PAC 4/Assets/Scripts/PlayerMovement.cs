18a51d439cd5c1456606ae914531eadcb65b2fc1
Naoise Golden Santos <naoise@uoc.edu>
2021-12-30 23:13:38 +0100

    Basic player movements

---
+++ PAC 4/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,85 @@
+using System;
+using UnityEngine;
+
+/* Script snippets from
+ * https://medium.com/nerd-for-tech/player-movement-in-unity-2d-using-rigidbody2d-4f6f1693d730
+ */
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField] private float playerSpeed = 5.0f;
+	[SerializeField] private float jumpPower = 5.0f;
+
+	private Rigidbody2D _rb;
+	private Animator _animator;
+	private SpriteRenderer _renderer;
+	private BoxCollider2D _collider;
+
+	private bool isGrounded = false;
+	private float dirX;
+
+	private void Awake()
+	{
+		_rb = GetComponent<Rigidbody2D>();
+		_animator = GetComponent<Animator>();
+		_renderer = GetComponent<SpriteRenderer>();
+		_collider = GetComponent<BoxCollider2D>();
+	}
+	private void Update()
+	{
+		MovePlayer();
+
+		if (Input.GetButton("Jump") && isGrounded)
+		{
+			Jump();
+		}
+
+		UpdateAnimation();
+	}
+	private void MovePlayer()
+	{
+		dirX = Input.GetAxisRaw("Horizontal");
+		_rb.velocity = new Vector2(dirX * playerSpeed, _rb.velocity.y);
+	}
+	private void Jump()
+	{
+		_rb.velocity = new Vector2(0, jumpPower);
+		isGrounded = false;
+	}
+
+	private void UpdateAnimation()
+	{
+		// Run animation
+		_animator.SetFloat("Speed", Mathf.Abs(dirX));
+
+		// Facing direction
+		if (dirX > 0)
+		{
+			_renderer.flipX = false;
+		}
+		else if (dirX < 0)
+		{
+			_renderer.flipX = true;
+		}
+
+		// Jump animations
+		if (_rb.velocity.y > 0.1f)
+		{
+			_animator.SetBool("IsJumping", true);
+		}
+		else if (_rb.velocity.y < -0.1f)
+		{
+			_animator.SetBool("IsJumping", false);
+			_animator.SetBool("IsFalling", true);
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+		{
+			// End jump
+			isGrounded = true;
+			_animator.SetBool("IsFalling", false);
+		}
+	}
+}
--- PAC 4/README.md
+++ PAC 4/README.md
@@ -2,6 +2,12 @@
 
 Author: Naoise Golden Santos <naoise@uoc.edu>
 
+## Implementation
+
+Pixel Perfect Camera.
+
+Pixels Per Unit set at 32.
+
 ## Resources
 
 * Free Pixel Font - Thaleah: https://assetstore.unity.com/packages/2d/fonts/free-pixel-font-thaleah-140059
